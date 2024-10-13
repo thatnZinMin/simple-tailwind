73a269cc63cbfcca00dff9f10cbb599b71c400e0
thant_zin_min <tm4557174@gmail.com>
2024-10-13 11:48:49 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      
+      thant: {
+        100: '#18181b',
+         200: '#27272a',
+         300:'#3f3f46',
+      },
+      zin:{
+        100:'#f1f5f9',
+        200:'#94a3b8',
+        300:'#07172a',
+      },
+      min:{
+        100:'#164e63',
+        600:'#2563eb',
+        200:'#000',
+        300:'#ffff'
+
+      }
+     
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
